c95396d48746be4b99513b1a83ad8c81670b6dc0
Favour Afripoint <favour.ezeh@afripointgroup.com>
2025-04-08 04:53:58 +0100

    update dashboard

--- src/app/modules/landing/components/dashboard/components/index.tsx
+++ src/app/modules/landing/components/dashboard/components/index.tsx
@@ -1,9 +1,9 @@
 import type React from "react"
 import DashboardHeader from "./DashboardHeader"
-import { useDashboardAnimation } from "../../../hooks/useDashboardAnimation"
+
 
 const Dashboard: React.FC = () => {
-  const { isSliding } = useDashboardAnimation()
+  
 
   return (
     <div className="mb-12">
