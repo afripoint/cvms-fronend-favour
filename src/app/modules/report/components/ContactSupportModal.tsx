124807db13b36f5dbda49538024116ebe263d0ba
Favour Afripoint <favour.ezeh@afripointgroup.com>
2025-05-07 14:38:43 +0100

    contact support screen update

--- src/app/modules/report/components/ContactSupportModal.tsx
+++ src/app/modules/report/components/ContactSupportModal.tsx
@@ -74,7 +74,7 @@ const ContactSupportModal = ({ isOpen, onClose }: ContactSupportModalProps) => {
     }
   };
 
-  const issueOptions = ["I paid customs duties", "System error", "Payment issue", "Delivery problem", "Other"];
+  const issueOptions = ["I paid customs duties", "System error", "Payment issue", "Other"];
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 p-4">
